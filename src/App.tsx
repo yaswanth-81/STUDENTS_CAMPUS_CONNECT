da509f7f91bb1d312f837148084c29f8113836ea
Sathwik <sathwikreddy0303@gmail.com>
2026-03-16 08:06:33 +0530

    landing issue

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,8 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/browse" element={<Browse />} />
